06c9b13a8760e4f39aa2f386de2702c502d56c1d
slowyier <nol37845@gmail.com>
2026-07-24 18:11:34 +0700

    fix:add advanced option in auth config

--- src/modules/auth/auth.config.ts
+++ src/modules/auth/auth.config.ts
@@ -56,7 +56,14 @@ export const auth = betterAuth({
   account: {
     encryptOAuthTokens: true,
   },
-  trustedOrigins: [env.cmsOrigin || 'http://localhost:3000', 'kuquest://'],
+  advanced: {
+  defaultCookieAttributes: {
+    sameSite: 'none',
+    secure: true,
+    httpOnly: true,
+  },
+  },
+  trustedOrigins: [env.cmsOrigin || 'http://localhost:5000', 'kuquest://', 'http://localhost:3000'],
 });
 
 export type AuthSession = typeof auth.$Infer.Session;
